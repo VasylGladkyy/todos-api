a3ebfa610e1f93a60f597a8b8b759aceb6a23eb4
Vasyl <vasil.gladkiy@gmail.com>
2020-09-02 20:36:17 +0300

    Annotate models (#1)
    * Add annotate gem
    
    * Annotate models

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: todos
+#
+#  id         :bigint           not null, primary key
+#  title      :string
+#  created_by :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Todo < ApplicationRecord
   has_many :items, dependent: :destroy
 
