a2f683256832546783dd83ff9debc1a2693d3359
Vasyl <vasil.gladkiy@gmail.com>
2020-09-02 20:34:29 +0300

    Annotate models

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: items
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  done       :boolean
+#  todo_id    :bigint           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Item < ApplicationRecord
   belongs_to :todo
 
