068bdea38146c9688ea20e1b11a83f3afeac0ef9
Vasyl <vasil.gladkiy@gmail.com>
2020-09-03 14:42:58 +0300

    Authenticate User

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -13,6 +13,6 @@ FactoryBot.define do
   factory :user do
     name { Faker::Name.name }
     email { Faker::Internet.email }
-    password_digest { '123456' }
+    password { '123456' }
   end
 end
