bc4c45cad7caaaed998ba2d7d4ad6c876013f7d8
Vasyl <vasil.gladkiy@gmail.com>
2020-09-03 17:19:05 +0300

    Implement user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :v1 do
     post 'auth/login', to: 'authentication#authenticate'
+    post 'signup', to: 'users#create'
 
     resources :todos do
       resources :items
