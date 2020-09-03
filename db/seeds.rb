95c06c4c3d2b66e829f5b04731b9fd250911e374
Vasyl <vasil.gladkiy@gmail.com>
2020-09-03 19:41:11 +0300

    Rewritten db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,6 @@
-FactoryBot.create_list(:todo, 10)
+user = FactoryBot.create(:user, email: 'todo.admin@gmail.com', password: '123456')
+
+FactoryBot.create_list(:todo, 10, created_by: user.id)
 
 Todo.all.each do |todo|
   FactoryBot.create_list(:item, 5, todo: todo)
